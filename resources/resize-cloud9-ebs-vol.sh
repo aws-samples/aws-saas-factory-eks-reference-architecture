757573e998f1d07972a2923ddcf86186ab835eb0
ranjith-aws <ranraman@amazon.com>
2021-01-08 18:37:28 -0800

    cloud9 instance resize script

--- resources/resize-cloud9-ebs-vol.sh
+++ resources/resize-cloud9-ebs-vol.sh
@@ -23,18 +23,35 @@ while [ \
 sleep 1
 done
 
+#Check if we're on an NVMe filesystem
 if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
 then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
- 
+
   # Expand the size of the file system.
-  sudo resize2fs /dev/xvda1
+  # Check if we are on AL2
+  STR=$(cat /etc/os-release)
+  SUB="VERSION_ID=\"2\""
+  if [[ "$STR" == *"$SUB"* ]]
+  then
+    sudo xfs_growfs -d /
+  else
+    sudo resize2fs /dev/xvda1
+  fi
 
 else
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/nvme0n1 1
 
   # Expand the size of the file system.
-  sudo resize2fs /dev/nvme0n1p1
+  # Check if we're on AL2
+  STR=$(cat /etc/os-release)
+  SUB="VERSION_ID=\"2\""
+  if [[ "$STR" == *"$SUB"* ]]
+  then
+    sudo xfs_growfs -d /
+  else
+    sudo resize2fs /dev/nvme0n1p1
+  fi
 fi
