79673141cb0d634b9e6d4936a2bbe534617f2b9a
ranjith-aws <ranraman@amazon.com>
2021-01-12 16:00:26 -0800

    user mgmt token processing changes-3

---
+++ services/shared-services/user-management-service/src/main/java/com/amazonaws/saas/eks/auth/TokenManager.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.amazonaws.saas.eks.auth;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+@Component
+public class TokenManager {
+	private static final Logger logger = LogManager.getLogger(TokenManager.class);
+
+	@Autowired
+	private JwtConfig jwtConfiguration;
+
+	private String getBearerToken(String token) {
+		return token.startsWith("Bearer ") ? token.substring("Bearer ".length()) : token;
+	}
+
+	public String extractUserPoolIdFromJwt(HttpServletRequest request) {
+		String userPoolId = null;
+		String idToken = request.getHeader(this.jwtConfiguration.getHttpHeader());
+
+		if (idToken != null) {
+			SignedJWT signedJWT = null;
+			JWTClaimsSet claimsSet = null;
+			
+			try {
+			    signedJWT = SignedJWT.parse(this.getBearerToken(idToken));
+				claimsSet = signedJWT.getJWTClaimsSet();			
+			} catch (java.text.ParseException e) {
+			    logger.error(e);
+			}
+
+			String issuer = claimsSet.getIssuer();
+			logger.info("issuer: " + issuer);
+
+			if(issuer!=null) {
+				int c = issuer.lastIndexOf("/");
+				userPoolId = issuer.substring(c+1);
+			}
+			logger.info("userPoolId: " + userPoolId);
+		}
+		return userPoolId;
+	}
+}
