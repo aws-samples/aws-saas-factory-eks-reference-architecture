fac26a906339100a8e4fd1a57211a7b9e06d6c21
Toby Buckley <tobuck@amazon.com>
2021-05-18 08:30:23 -0700

    Auth Interceptor Fix Now excludes Amazon Cognito URLs from the list of endpoints that get the ID token attached. #fixes 18.

--- clients/Admin/src/app/interceptors/auth.interceptor.ts
+++ clients/Admin/src/app/interceptors/auth.interceptor.ts
@@ -19,27 +19,31 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor(private oidcSecurityService: OidcSecurityService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const token = this.oidcSecurityService.getIdToken();
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = this.oidcSecurityService.getIdToken();
 
-      if (!token) {
-          return next.handle(req);
-      }
-
-      req = req.clone({
-          headers: req.headers.set('Authorization', 'Bearer ' + token),
-      });
-
+    // We only want to add the ID Token to our API endpoints. Let the Auth library handle
+    // tokens for the Cognito endpoints
+    if (!token || req.url.includes('amazoncognito')) {
       return next.handle(req);
+    }
+
+    req = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + token),
+    });
+
+    return next.handle(req);
   }
 }
