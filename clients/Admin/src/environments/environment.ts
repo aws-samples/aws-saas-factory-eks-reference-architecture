9b74076240e5be50b30dd297aee2df8105888ff8
Toby Buckley <tobuck@amazon.com>
2021-09-30 13:20:13 -0700

    Fix logout

--- clients/Admin/src/environments/environment.ts
+++ clients/Admin/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  clientId: '2j5qgn5ejbgdh06ibsj4f2d4qv',
-  issuer: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_C6nwHKeXW',
-  apiUrl: 'https://api.eks-ref-arch.com',
-  domain: 'eks-ref-arch.com'
+  clientId: '$APPCLIENTID',
+  issuer: '$AUTHSERVERURL',
+  customDomain: '$AUTHCUSTOMDOMAIN',
+  apiUrl: 'https://api.$CUSTOM_DOMAIN',
+  domain: '$CUSTOM_DOMAIN',
 };
