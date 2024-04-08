c3892b1fdf62c00eb46505d81d4fb5cefcf79cbd
Toby Buckley <tobuck@amazon.com>
2024-04-08 15:28:55 -0700

    Prettier

--- clients/projects/application/src/app/views/pages/routes.ts
+++ clients/projects/application/src/app/views/pages/routes.ts
@@ -3,30 +3,30 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '404',
-    loadComponent: () => import('./page404/page404.component').then(m => m.Page404Component),
+    loadComponent: () => import('./page404/page404.component').then((m) => m.Page404Component),
     data: {
-      title: 'Page 404'
-    }
+      title: 'Page 404',
+    },
   },
   {
     path: '500',
-    loadComponent: () => import('./page500/page500.component').then(m => m.Page500Component),
+    loadComponent: () => import('./page500/page500.component').then((m) => m.Page500Component),
     data: {
-      title: 'Page 500'
-    }
+      title: 'Page 500',
+    },
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
     data: {
-      title: 'Login Page'
-    }
+      title: 'Login Page',
+    },
   },
   {
     path: 'register',
-    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent),
+    loadComponent: () => import('./register/register.component').then((m) => m.RegisterComponent),
     data: {
-      title: 'Register Page'
-    }
-  }
+      title: 'Register Page',
+    },
+  },
 ];
