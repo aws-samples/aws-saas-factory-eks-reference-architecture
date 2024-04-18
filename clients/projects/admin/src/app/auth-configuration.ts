35207ff5648cb880097dfaad2e0241451adf725c
Toby Buckley <tobuck@amazon.com>
2024-04-18 15:34:32 -0700

    Changed config

--- clients/projects/admin/src/app/auth-configuration.ts
+++ clients/projects/admin/src/app/auth-configuration.ts
@@ -4,7 +4,8 @@ import { environment } from '../environments/environment';
 
 export const ConfigLoaderFactory = () => {
   return new StsConfigStaticLoader({
-    authority: environment.issuer,
+    authority: environment.authServer,
+    authWellknownEndpointUrl: environment.wellKnownEndpointUrl,
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: `${window.location.origin}/signout`,
     clientId: environment.clientId,
